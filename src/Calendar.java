7a573fb136ec5172fccadea7e86ae1d3ba1dd1ac
Nate <nawe6601@colorado.edu>
2017-04-25 12:35:19 -0600

    Addded Calendar class

---
+++ src/Calendar.java
@@ -0,0 +1,46 @@
+public class Calendar {
+	
+	protected User owner;
+	protected String permissions;
+	protected Event[] events;
+	
+	User getOwner() {
+		return owner;
+	}
+
+	void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	String getPermissions() {
+		return permissions;
+	}
+
+	void setPermissions(String permissions) {
+		this.permissions = permissions;
+	}
+
+	Event[] getEvents() {
+		return events;
+	}
+
+	void setEvents(Event[] events) {
+		this.events = events;
+	}
+
+	public Calendar() {
+	}
+
+	public Calendar(User owner)
+	{
+		this.owner = owner;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
